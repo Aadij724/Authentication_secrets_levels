18df5c3926813544d1055f99fc42d87df8513a49
Aadi Jain <aadij4945@gmail.com>
2023-08-06 12:36:06 +0530

    Level 1 complete

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const mongoose = require("mongoose");
 
 const app = express();
 
@@ -10,6 +11,15 @@ app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({
     extended: true
 }));
+
+mongoose.connect("mongodb://0.0.0.0:27017/userDB", );
+
+const userSchema = new mongoose.Schema({
+    email: String,
+    password: String
+});
+
+const User = mongoose.model("User", userSchema);
 
 
 app.get("/", function(req, res) {
@@ -24,9 +34,56 @@ app.get("/login", function(req, res) {
     res.render("login");
 })
 
+app.post("/register", function(req, res) {
+    const newUser = new User({
+        email: req.body.username,
+        password: req.body.password
+    });
+
+    try {
+        newUser.save();
+    } catch(err) {
+        console.log(err);
+    }
+
+    res.render("secrets");
+})
+
+
+app.post("/login", function(req, res) {
+    const readDb = async () => {
+
+        try {
+            const user = await User.findOne({email : req.body.username}).exec();
+
+            if (user) {
+                if(req.body.password == user.password) {
+                    res.render("secrets");
+                } else {
+                    console.log("Wrong password");
+                    res.redirect("/login");
+                }
+            } else {
+                console.log("No such username");
+                res.redirect("/login");
+            }
+        } catch(err) {
+            console.log(err);
+            res.redirect("/login");
+        }
+        
+    }
+
+    readDb();
+        
+})
+
 
 
 
 app.listen( 3000, function() {
     console.log("Server started on port 3000");
 })
+
+
+
