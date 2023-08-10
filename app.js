bedca4ac724b15887b095dc3f9cb2e6b00640ef8
Aadi Jain <aadij4945@gmail.com>
2023-08-10 08:39:39 +0530

    Secrets Completed

--- app.js
+++ app.js
@@ -33,7 +33,8 @@ mongoose.connect("mongodb://0.0.0.0:27017/userDB", );
 const userSchema = new mongoose.Schema({
     googleId: String,
     email: String,
-    password: String
+    password: String,
+    secret: String
 });
 
 userSchema.plugin(passportLocalMongoose);
@@ -91,13 +92,51 @@ app.get("/auth/google/secrets",
     res.redirect('/secrets');
 });
 
-app.get("/secrets", function(req, res) {
+app.get("/secrets",async function(req, res) {
+    try {
+        const foundUsers = await User.find({ "secret": {$ne: null}}).exec();
+    
+        if(foundUsers) {
+            res.render("secrets", {usersWithSecrets: foundUsers});
+        }
+    }
+    catch(err) {
+        console.log(err);
+    }
+    
+});
+
+app.get("/submit", function(req, res) {
     if(req.isAuthenticated()) {
-        res.render("secrets");
+        res.render("submit");
     } else {
         res.redirect("/login");
     }
 });
+
+app.post("/submit", async function(req, res) {
+    const submittedSecret = req.body.secret;
+
+    console.log(req.user);
+
+    try {
+        const foundUser = await User.findById(req.user.id).exec();
+
+        if (foundUser) {
+            foundUser.secret = submittedSecret;
+            await foundUser.save();
+            console.log("Secret saved successfully");
+        } else {
+            console.log("User not found, secret not saved");
+        }
+
+        res.redirect("/secrets"); // Redirect to the secrets page or another appropriate destination
+    } catch (error) {
+        console.error("Error saving secret:", error);
+        res.status(500).send("An error occurred while saving the secret.");
+    }
+});
+
 
 
 app.get("/logout", function(req, res) {
